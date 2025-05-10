365cd1b1c895b315f0e6ede5e27ee66d44612b99
ThalesFortes <thales_pro@outlook.com>
2025-05-09 23:59:20 -0300

    Catch the currency

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
+// Obtendo os elementos do formulario.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulando o input para receber somente numeros
-amount.addEventListener("input", (event) => {
+amount.addEventListener("input", () => {
   const hasCharacterRegex = /\D+/g
   amount.value = amount.value.replace(hasCharacterRegex, "")
 })
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(currency.value)
+})
+irundina222@uorak.com
