58b76de7aaa1727bc88839ae49915d8b4f84b9a5
ThalesFortes <thales_pro@outlook.com>
2025-05-11 00:00:29 -0300

    Identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas do dias
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
 // Obtendo os elementos do formulario.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -11,5 +16,22 @@ amount.addEventListener("input", () => {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(currency.value)
+  
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "EUR":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
 })
+
+
+// Função para converter a moeda
+function convertCurrency (amount, price, symbol){
+  
+}
